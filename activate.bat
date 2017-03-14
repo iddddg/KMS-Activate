96aac2e65a1fb3c17c317eebef423c5da6e3683d
iddddg <iddddg@qq.com>
2017-03-14 09:45:49 +0800

    Update activate.bat
    双击打开,自动获取管理员权限,不用再麻烦的右键,管理员模式运行了:)

--- activate.bat
+++ activate.bat
@@ -1,5 +1,27 @@
 @echo off
 mode con cols=100 lines=30 & color fc
+
+rem 获取管理员权限
+:: BatchGotAdmin  
+:-------------------------------------  
+rem  --> Check for permissions  
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+rem --> If error flag set, we do not have admin.  
+if '%errorlevel%' NEQ '0' (  
+    echo Requesting administrative privileges...  
+    goto UACPrompt  
+) else ( goto gotAdmin )   
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"   
+    "%temp%\getadmin.vbs"
+    exit /B  
+:gotAdmin  
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
+    pushd "%CD%"  
+    CD /D "%~dp0"
+
+rem 激活部分
 cls
 :start
 echo\
